f6da1a0b5a004829614df03ab6caaf7fe9904f08
patrycjusz.blaszczuk <patrycjusz.blaszczuk@gmail.com>
2020-05-04 19:23:16 +0200

    Executing js based on loaded fragment page name, sample js chart

--- src/main/resources/static/resources/js/utils.js
+++ src/main/resources/static/resources/js/utils.js
@@ -1,11 +1,28 @@
-function showAlert() {
-    alert("Alert");
+function pageLoaded(pageName) {
+    if (pageName === "allWeighings") {
+        loadWeighingChart();
+    }
 }
 
-function showAlertWithParam(text) {
-    alert(text);
-}
-
-function getSomeString() {
-    return "some string";
+function loadWeighingChart() {
+    var chart = new CanvasJS.Chart("weighingsChartContainer", {
+        theme: "light1", // "light2", "dark1", "dark2"
+        animationEnabled: false, // change to true
+/*        title: {
+            text: "Basic Column Chart"
+        },*/
+        data: [
+            {
+                type: "column",
+                dataPoints: [
+                    {label: "apple", y: 10},
+                    {label: "orange", y: 15},
+                    {label: "banana", y: 25},
+                    {label: "mango", y: 30},
+                    {label: "grape", y: 28}
+                ]
+            }
+        ]
+    });
+    chart.render();
 }
